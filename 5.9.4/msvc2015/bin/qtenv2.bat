82216455cfaaf2c0232f013f29ccfd1905424647
CsloudX <35988767+CsloudX@users.noreply.github.com>
2018-11-07 22:27:43 +0800

    V1

---
+++ 5.9.4/msvc2015/bin/qtenv2.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Setting up environment for Qt usage...
+set PATH=D:\Qt\Qt5.9.4\5.9.4\msvc2015\bin;%PATH%
+cd /D D:\Qt\Qt5.9.4\5.9.4\msvc2015
+echo Remember to call vcvarsall.bat to complete environment setup!
